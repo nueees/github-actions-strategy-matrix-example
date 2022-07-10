f20eeb162e1f0ea2a3f6ff4054be92f925f797cb
siena <nueees@gmail.com>
2022-07-10 13:13:26 +0200

    test

---
+++ lambda/opsgenie_test/opsgenie_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from datetime import date, timedelta
+from datetime import datetime
+
+def handler(event, context):
+    print('opsgenie_export_lambda.start')
+
+if __name__ == '__main__':
+    handler(None, None)
